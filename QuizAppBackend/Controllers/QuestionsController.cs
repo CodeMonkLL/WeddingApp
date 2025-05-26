838c0bb91d82a9d2f927648582eea735c535b6e2
Lukas Luemers <lukas.luemers@gdata.de>
2025-05-26 20:26:57 +0200

    routes for usercontroller and questionscontroller

--- QuizAppBackend/Controllers/QuestionsController.cs
+++ QuizAppBackend/Controllers/QuestionsController.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Quizappbackend.Data;
@@ -15,5 +16,47 @@ namespace Quizappbackend.Controllers
             _context = context;
         }
 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Question>>> GetQuestions()
+        {
+            return await _context.Questions.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Question>> GetQuestion(int id)
+        {
+            var question = await _context.Questions.FindAsync(id);
+
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(question);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Question>> PostQuestion(Question question)
+        {
+            _context.Questions.Add(question);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteQuestion(int id)
+        {
+            var question = await _context.Questions.FindAsync(id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            _context.Questions.Remove(question);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
---
+++ QuizAppBackend/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Quizappbackend.Data;
+using Quizappbackend.Models;
+
+namespace Quizappbackend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class Usercontroller : ControllerBase
+{
+    private readonly QuizDbContext _context;
+
+    public Usercontroller(QuizDbContext context)
+    {
+        _context = context;
+    }
+
+
+    [HttpPost]
+    public async Task<ActionResult<UserResult>> PostUser(UserResult user)
+    {
+        var existingUser = await _context.UserResults
+        .FirstOrDefaultAsync(u => u.Username == user.Username);
+
+        if (existingUser != null)
+        {
+            return Conflict($"Username '{user.Username}' already exists.");
+        }
+
+        _context.UserResults.Add(user);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetUserByName), new { user = user.Id }, user);
+    }
+
+    [HttpGet("User")]
+    public async Task<ActionResult<UserResult>> GetUserByName(string name)
+    {
+        var user = await _context.UserResults.FirstOrDefaultAsync(u => u.Username == name);
+
+        if (user == null)
+        {
+            return NotFound($"User not found :{name}");
+        }
+
+        return Ok(user);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<UserResult>> GetAllUsers()
+    {
+        List<UserResult> listOfUsers = await _context.UserResults.ToListAsync();
+        return Ok(listOfUsers);
+    }
+}
