c2caeaa1bdfd9d1254601d1f38d05285dd38a48f
Lukas Luemers <lukas.luemers@gdata.de>
2025-06-10 07:47:55 +0200

    Add Dockerfile, migrations, .gitignore; remove bin/Debug artifacts

--- QuizAppBackend/Program.cs
+++ QuizAppBackend/Program.cs
@@ -5,15 +5,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 // SQLite-Konfiguration
 builder.Services.AddDbContext<QuizDbContext>(options =>
-    options.UseSqlite("Data Source=quiz.db"));
+    options.UseSqlite("Data Source=/app/data/quiz.db"));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+// Migration beim Start ausführen
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<QuizDbContext>();
+    db.Database.Migrate();
+}
+
+
+// app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
