9a73eadf5913336aadfa8d3e3db22322425ee3c5
Lukas Luemers <lukas.luemers@gdata.de>
2025-07-13 20:45:11 +0200

    edited the navbar links to make them work for views and components

--- QuizAppFrontend/src/app/Views/home/home.component.ts
+++ QuizAppFrontend/src/app/Views/home/home.component.ts
@@ -2,18 +2,13 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../Components/navbar/navbar.component';
 import { QuizComponent } from '../../Components/quiz/quiz.component';
 import { NgIf } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, NgIf],
+  imports: [NavbarComponent, NgIf, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
 })
-export class HomeComponent {
-  isOpen = false;
-
-  toggle() {
-    this.isOpen = !this.isOpen;
-  }
-}
+export class HomeComponent {}
